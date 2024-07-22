6e130fa2b5080f7c6af71382a32a257af54a317a
zheshiluanma <35163699+zheshiluanma@users.noreply.github.com>
2024-07-22 19:37:37 +0800

    首次提交

---
+++ Assets/CorgiEngine/Common/Scripts/Agents/AI/Advanced/AIActionCrouchStart.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.Tools;
+
+namespace MoreMountains.CorgiEngine
+{
+	/// <summary>
+	/// This action will cause your AI character to start crouching
+	/// </summary>
+	[AddComponentMenu("Corgi Engine/Character/AI/Actions/AI Action Crouch Start")]
+	public class AIActionCrouchStart : AIAction
+	{
+		protected CharacterCrouch _characterCrouch;
+
+		/// <summary>
+		/// On init we grab our CharacterRun component
+		/// </summary>
+		public override void Initialization()
+		{
+			if(!ShouldInitialize) return;
+			_characterCrouch = this.gameObject.GetComponentInParent<Character>()?.FindAbility<CharacterCrouch>();
+		}
+
+		/// <summary>
+		/// On PerformAction we start running
+		/// </summary>
+		public override void PerformAction()
+		{
+			_characterCrouch.Crouch();
+		}
+	}
+}
---
+++ Assets/CorgiEngine/Common/Scripts/Agents/AI/Advanced/AIActionRunStart.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.Tools;
+
+namespace MoreMountains.CorgiEngine
+{
+	/// <summary>
+	/// This action will cause your AI character to start running
+	/// </summary>
+	[AddComponentMenu("Corgi Engine/Character/AI/Actions/AI Action Run Start")]
+	public class AIActionRunStart : AIAction
+	{
+		protected CharacterRun _characterRun;
+
+		/// <summary>
+		/// On init we grab our CharacterRun component
+		/// </summary>
+		public override void Initialization()
+		{
+			if(!ShouldInitialize) return;
+			_characterRun = this.gameObject.GetComponentInParent<Character>()?.FindAbility<CharacterRun>();
+		}
+
+		/// <summary>
+		/// On PerformAction we start running
+		/// </summary>
+		public override void PerformAction()
+		{
+			_characterRun.RunStart();
+		}
+	}
+}
