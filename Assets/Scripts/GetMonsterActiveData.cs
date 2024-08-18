ad0a462d9d1fc7a449a46a5d0b2b9de0fe2500f5
zheshiluanma <35163699+zheshiluanma@users.noreply.github.com>
2024-08-18 23:17:16 +0800

    1111

--- Assets/Scripts/GetMonsterActiveData.cs
+++ Assets/Scripts/GetMonsterActiveData.cs
@@ -10,17 +10,36 @@ public class GetMonsterActiveData : MonoBehaviour
 {
     public ActiveData MonsterData;
     public string id;
+    public bool isBoss;
     private void Awake()
     {
-        //var id=gameObject.name.Replace("Variant","");
-        Debug.Log(id);
-        var data = DataMgr.Instance.GetEnemyData(id);
-        MonsterData = data;
-        var health = GetComponent<Health>();
-        health.MaximumHealth = data.Hp;
-        health.InitialHealth = data.Hp;
-        health.InitializeCurrentHealth();
-        health.Dfs = data.Dfs;
-        //GetComponent<weap>()
+        if (isBoss)
+        {
+            //var id=gameObject.name.Replace("Variant","");
+            Debug.Log(id);
+            var health = GetComponent<Health>();
+        
+            health.MaximumHealth =1000;
+            health.InitialHealth = 1000;
+            health.InitializeCurrentHealth();
+            health.Dfs =30;
+            MonsterData = new ActiveData(0, "boss", ActiveType.头目, 1000, 30, 100, 1, 1, 1);
+
+            //GetComponent<weap>()
+        }
+        else
+        {
+            //var id=gameObject.name.Replace("Variant","");
+            Debug.Log(id);
+            var data = DataMgr.Instance.GetEnemyData(id);
+            MonsterData = data;
+            var health = GetComponent<Health>();
+            health.MaximumHealth = data.Hp;
+            health.InitialHealth = data.Hp;
+            health.InitializeCurrentHealth();
+            health.Dfs = data.Dfs;
+            //GetComponent<weap>()
+        }
+      
     }
 }
